2600fad50820f170e0912fbf7868b19fde52e335
Cristian Marques <46373143+CristianMarques97@users.noreply.github.com>
2019-04-24 15:40:18 -0300

    fix

--- Store/Front/new-pstore/src/index.js
+++ Store/Front/new-pstore/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';import {createStore} from 'redux';
+import * as serviceWorker from './serviceWorker';
+// import {createStore} from 'redux';
 
 // function reducer(state= [], action) {
 //     return State;
